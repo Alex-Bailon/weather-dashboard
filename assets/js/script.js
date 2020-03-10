a4d27e9a067f9befd0971d22164768a5046654c2
Alex B <abailon949@gmail.com>
2020-03-10 14:41:30 -0500

    Added code for UV index in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,18 +5,40 @@ $(document).ready(function(){
             units: 'imperial',
             appid: '4c56e5acd3376fa64faa9d6655738d01'
         }
-            let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?' + $.param(options)
-            console.log(queryURL)
+        let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?' + $.param(options)
+        console.log(queryURL)
+        $.ajax({
+            url: queryURL,
+            method: "GET"
+        }).then(function(response){
+            let iconimg = response.list[0].weather[0].icon
+            let iconURL = `http://openweathermap.org/img/w/${iconimg}.png`
+            $( '#city' ).html(`${ response.city.name } (${ response.list[0].dt_txt.split(' ')[0] }) <img src='${ iconURL }'/>`)
+            $( '#temp').text(`Temperature: ${ response.list[0].main.temp } °F`)
+            $( '#humidity' ).text(`Humidity: ${ response.list[0].main.humidity }%`)
+            $( '#wind' ).text(`Wind Speed: ${ response.list[0].wind.speed } MPH`)
+            let cord = {
+                lat: response.city.coord.lat,
+                lon: response.city.coord.lon,
+                appid: '4c56e5acd3376fa64faa9d6655738d01'
+            }
+            let uvURL = 'http://api.openweathermap.org/data/2.5/uvi?' + $.param(cord)
             $.ajax({
-                url: queryURL,
+                url: uvURL,
                 method: "GET"
-            }).then(function(response){
-                $( '#city' ).text(`${ response.city.name } (${ response.list[0].dt_txt.split(' ')[0] })`)
-                $( '#temp').text(`Temperature: ${ response.list[0].main.temp } °F`)
-                $( '#humidity' ).text(`Humidity: ${ response.list[0].main.humidity }%`)
-                $( '#wind' ).text(`Wind Speed: ${ response.list[0].wind.speed } MPH`)
+            }).then(function(res){
+                uvIndex = res.value
+                $( '#uv' ).html(`UV index: <span id="uvIndex">${ uvIndex }</span>`)
+                if ( uvIndex < 3 ){
+                    $( '#uvIndex' ).css('background-color', 'green')
+                }
+                else if ( uvIndex >= 3 && uvIndex <= 6 ){
+                    $( '#uvIndex' ).css('background-color', 'orange')
+                }
+                else{
+                    $( '#uvIndex' ).css('background-color', 'red')
+                }
             })
+        })
     })
-
-
 })
