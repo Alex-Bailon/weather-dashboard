ac610b0556413c84cc43d6af7b12ba9e3f9f4cf6
Alex B <abailon949@gmail.com>
2020-03-11 09:37:17 -0500

    Added 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
     let historyArray = []
+    let weatherResponse = []
+    let UVResponse = []
     $( '#searchBtn' ).on('click', function(){
         let options = {
             q: $( '#cityInput' ).val(),
@@ -12,6 +14,7 @@ $(document).ready(function(){
             url: queryURL,
             method: "GET"
         }).then(function(response){
+            weatherResponse.unshift(response)
             let iconimg = response.list[0].weather[0].icon
             let iconURL = `http://openweathermap.org/img/w/${iconimg}.png`
             $( '#city' ).html(`${ response.city.name } (${ response.list[0].dt_txt.split(' ')[0] }) <img src='${ iconURL }'/>`)
@@ -29,6 +32,7 @@ $(document).ready(function(){
                 method: "GET"
             }).then(function(res){
                 uvIndex = res.value
+                UVResponse.unshift(uvIndex)
                 $( '#uv' ).html(`UV index: <span id="uvIndex">${ uvIndex }</span>`)
                 if ( uvIndex < 3 ){
                     $( '#uvIndex' ).css('background-color', 'green')
@@ -45,9 +49,29 @@ $(document).ready(function(){
             console.log(historyArray)
             historyArray.forEach((item) => {
                 console.log(item)
-                let historyDiv = $( `<p> ${ item} </p>`)
+                let historyDiv = $( `<p class="hisItem"> ${ item} </p>`)
                 $( '#history' ).append(historyDiv)
-        })
+            })
+                
+            $( '#fiveDayText' ).css('display', 'block')
+            $( '#fiveDay' ).text('')
+            for ( let i = 7; i < 48; i+=8 ){
+                console.log(i)
+                let fiveDayIcon = response.list[i].weather[0].icon
+                let fiveDayURL = `http://openweathermap.org/img/w/${fiveDayIcon}.png`
+                let card = $( 
+                    `<div class="col s3">
+                        <div class="card-panel light-blue">
+                            <span class="white-text" id="spanDate">${ response.list[i].dt_txt.split(' ')[0] }</span>
+                            <img src='${ fiveDayURL }'/>
+                            <p>Temp: ${ response.list[i].main.temp } °F</p>
+                            <p>Humidity: ${ response.list[i].main.humidity }%</p>
+                        </div>
+                    </div>`
+                )
+                $( '#fiveDay' ).append(card)
+             }
         })
     })
+
 })
