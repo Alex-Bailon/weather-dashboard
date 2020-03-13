9ee7ccba4c04fa619bfd72d2580b0a558dc54e12
Alex B <abailon949@gmail.com>
2020-03-13 15:08:17 -0400

    Added arrays to local storage to save seach history

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,77 @@
+let h = 0
+let historyArray = []
+let weatherResponse = []
+let UVResponse = []
+init()
+inith()
+initUV()
+initHis()
 $(document).ready(function(){
-    let historyArray = []
-    let weatherResponse = []
-    let UVResponse = []
-    $( '#searchBtn' ).on('click', function(){
+    if (h != 0){
+        h = 0
+        historyArray.forEach((item) => {
+            let historyDiv = $( `<p id="${ h }" class="hisItem"> ${ item } </p>`)
+            $( '#history' ).append(historyDiv)
+            h++
+        })
+    }
+    $( '.hisItem' ).on('click', function(){
+        let clickedVal = +($(this).attr('id'))
+        let uvClicked = UVResponse[clickedVal]
+        $( '#uv ').html('')
+        $( '#uv' ).html(`UV index: <span id="uvIndex">${ uvClicked }</span>`)
+        if ( uvClicked < 3 ){
+            $( '#uvIndex' ).css('background-color', 'green')
+        }
+        else if ( uvClicked >= 3 && uvClicked <= 6 ){
+            $( '#uvIndex' ).css('background-color', 'orange')
+        }
+        else{
+            $( '#uvIndex' ).css('background-color', 'red')
+        }
+        renderCurrentDay(weatherResponse[clickedVal]) 
+    })
+    $( '#input' ).on('submit', function(){
+        event.preventDefault()
         let options = {
             q: $( '#cityInput' ).val(),
             units: 'imperial',
             appid: '4c56e5acd3376fa64faa9d6655738d01'
         }
-        historyArray.unshift(options.q)
-        $( '#history' ).html('')
-        let i = 0
-        historyArray.forEach((item) => {
-            let historyDiv = $( `<p id="${ i }" class="hisItem"> ${ item } </p>`)
-            $( '#history' ).append(historyDiv)
-            i++
-        })
         let queryURL = 'https://api.openweathermap.org/data/2.5/forecast?' + $.param(options)
         $.ajax({
             url: queryURL,
             method: "GET"
         }).then(function(response){
+            historyArray.unshift(options.q)
+            localStorage.setItem('historyArray', JSON.stringify(historyArray))
+            $( '#history' ).html('')
+            h = 0
+            historyArray.forEach((item) => {
+                let historyDiv = $( `<p id="${ h }" class="hisItem"> ${ item } </p>`)
+                $( '#history' ).append(historyDiv)
+                h++
+                localStorage.setItem('h', h)
+            })
+            $( '.hisItem' ).on('click', function(){
+                console.log('clicked')
+                let clickedVal = +($(this).attr('id'))
+                let uvClicked = UVResponse[clickedVal]
+                $( '#uv ').html('')
+                $( '#uv' ).html(`UV index: <span id="uvIndex">${ uvClicked }</span>`)
+                if ( uvClicked < 3 ){
+                    $( '#uvIndex' ).css('background-color', 'green')
+                }
+                else if ( uvClicked >= 3 && uvClicked <= 6 ){
+                    $( '#uvIndex' ).css('background-color', 'orange')
+                }
+                else{
+                    $( '#uvIndex' ).css('background-color', 'red')
+                }
+                renderCurrentDay(weatherResponse[clickedVal]) 
+            })    
             weatherResponse.unshift(response)
+            localStorage.setItem('weatherResponse', JSON.stringify(weatherResponse))
             let cord = {
                 lat: response.city.coord.lat,
                 lon: response.city.coord.lon,
@@ -32,8 +82,10 @@ $(document).ready(function(){
                 url: uvURL,
                 method: "GET"
             }).then(function(res){
+                console.log('uv ajax')
                 let uvIndex = res.value
                 UVResponse.unshift(uvIndex)
+                localStorage.setItem('UVResponse', JSON.stringify(UVResponse))
                 $( '#uv' ).html(`UV index: <span id="uvIndex">${ uvIndex }</span>`)
                 if ( uvIndex < 3 ){
                     $( '#uvIndex' ).css('background-color', 'green')
@@ -44,35 +96,43 @@ $(document).ready(function(){
                 else{
                     $( '#uvIndex' ).css('background-color', 'red')
                 }
+                console.log('uv end')
             })
-            renderCurrentDay(response)
-            
-            
-        })
-        $( '.hisItem' ).on('click', function(){
-            let clickedVal = +($(this).attr('id'))
-            let uvClicked = UVResponse[clickedVal]
-            $( '#uv ').html('')
-            $( '#uv' ).html(`UV index: <span id="uvIndex">${ uvClicked }</span>`)
-            if ( uvClicked < 3 ){
-                $( '#uvIndex' ).css('background-color', 'green')
-            }
-            else if ( uvClicked >= 3 && uvClicked <= 6 ){
-                $( '#uvIndex' ).css('background-color', 'orange')
-            }
-            else{
-                $( '#uvIndex' ).css('background-color', 'red')
-            }
-            renderCurrentDay(weatherResponse[clickedVal])
-            
+            renderCurrentDay(response)  
         })
     })
-    
-    
 })
 
+function init(){
+    var retrive = JSON.parse(localStorage.getItem('weatherResponse'))
+    if (retrive !== null){
+        weatherResponse = retrive
+    }
+}
+
+function inith(){
+    var retrive = JSON.parse(localStorage.getItem('h'))
+    if (retrive !== null){
+        h = retrive
+    }
+}
+
+function initUV(){
+    var retrive = JSON.parse(localStorage.getItem('UVResponse'))
+    if (retrive !== null){
+        UVResponse = retrive
+    }
+}
+
+function initHis(){
+    var retrive = JSON.parse(localStorage.getItem('historyArray'))
+    if (retrive !== null){
+        historyArray = retrive
+    }
+}
 
 function renderCurrentDay(response){
+    $( '#fiveDayText' ).css('display', 'block')
     let iconimg = response.list[0].weather[0].icon
     let iconURL = `http://openweathermap.org/img/w/${iconimg}.png`
     $( '#city' ).html(`${ response.city.name } (${ response.list[0].dt_txt.split(' ')[0] }) <img src='${ iconURL }'/>`)
@@ -80,7 +140,7 @@ function renderCurrentDay(response){
     $( '#humidity' ).text(`Humidity: ${ response.list[0].main.humidity }%`)
     $( '#wind' ).text(`Wind Speed: ${ response.list[0].wind.speed } MPH`)
     $( '#fiveDay' ).text('')
-    for ( let i = 7; i < 48; i+=8 ){
+    for ( let i = 7; i < 40; i+=8 ){
         let fiveDayIcon = response.list[i].weather[0].icon
         let fiveDayURL = `http://openweathermap.org/img/w/${fiveDayIcon}.png`
         let card = $( 
@@ -94,6 +154,5 @@ function renderCurrentDay(response){
             </div>`
         )
         $( '#fiveDay' ).append(card)
-        $( '#fiveDayText' ).css('display', 'block')
     }
 }
